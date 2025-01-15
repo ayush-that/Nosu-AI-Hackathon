bf8443a0beda7a882fff52cbebd171495ad48f84
MohitGoyal09 <goel0632@gmail.com>
2025-01-15 16:21:44 +0530

    Refactor code to remove loading component and update user options in MyStatsig component

--- lib/my-statsig.tsx
+++ lib/my-statsig.tsx
@@ -8,9 +8,7 @@ export default function MyStatsig({ children }: { children: React.ReactNode }) {
   return (
     <StatsigProvider
       sdkKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
-      loadingComponent={
-        <div style={{ height: 100, width: 300, padding: 16 }}>Loading...</div>
-      }
+      
       user={{ userID: "optional" }}
       options={{
         logLevel: LogLevel.Debug,
